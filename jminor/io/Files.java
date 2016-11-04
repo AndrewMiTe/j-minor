bae1b4b673b56ea3fa0a69830d5e2deccaed24e0
Andrew Michael Teller <andrew.m.teller@gmail.com>
2016-11-03 21:58:54 -0400

    Adds implimentation of objects() method.

--- jminor/io/Files.java
+++ jminor/io/Files.java
@@ -24,17 +24,91 @@
 
 package jminor.io;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  *
  * @author Andrew M. Teller(https://github.com/AndrewMiTe)
  */
 public class Files {
+  
+  public static final Stream<Object> objects(Path path) throws IOException {
+    ObjectFileIterator fileObjects = new ObjectFileIterator(path);
+    return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+        fileObjects, Spliterator.IMMUTABLE), false)
+        .onClose(() -> fileObjects.close())
+    ;
+  }
+  
+  private static class ObjectFileIterator implements Iterator<Object> {
 
-  public static final Stream<Object> objects(Path path) {
-    throw new UnsupportedOperationException("Not supported yet.");
+    private ObjectInputStream input = null;
+    
+    private Path path;
+    
+    private Object nextObject = null;
+
+    public ObjectFileIterator(Path path) {
+      this.path = path;
+    }
+    
+    private void openInput() {
+      try {
+        FileInputStream fileIn = new FileInputStream(path.toFile());
+        BufferedInputStream bufferedIn = new BufferedInputStream(fileIn);
+        input = new ObjectInputStream(bufferedIn);
+        nextObject = input.readObject();
+      }
+      catch (IOException | ClassNotFoundException e) {
+        nextObject = null;
+        try {
+          input.close();
+        }
+        catch (IOException f) {
+        }
+      }
+    }
+    
+    public void close() {
+      try {
+        input.close();
+      }
+      catch (IOException e) {        
+      }
+    }
+    
+    @Override // from Iterator
+    public boolean hasNext() {
+      if (input == null) openInput();
+      return nextObject != null;
+    }
+
+    @Override // from Iterator
+    public Object next() {
+      Object returnValue = nextObject;
+      nextObject = null;
+      try {
+        nextObject = input.readObject();
+      }
+      catch (IOException | ClassNotFoundException e) {
+        try {
+          input.close();
+        }
+        catch (IOException f) {
+        }
+      }
+      return returnValue;
+    }
+    
   }
   
 }
